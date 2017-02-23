c919a7031fa757584c4e6287b8112c4ccda4d6e4
Prateek Jain <prateek.jain23995@yahoo.com>
2017-02-23 17:34:54 +0530

    making library independent of spring or any other network libary.
    - this will make sure we can use AndroidSnooper with any other network library also

--- Snooper/src/androidTest/java/com/prateekj/snooper/AndroidSnooperTest.java
+++ Snooper/src/androidTest/java/com/prateekj/snooper/AndroidSnooperTest.java
@@ -1,30 +1,17 @@
 package com.prateekj.snooper;
 
+import com.prateekj.snooper.HttpCall.HttpCallBuilder;
 import com.prateekj.snooper.rules.RealmCleanRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-
-import java.io.ByteArrayInputStream;
-import java.net.URI;
 
 import io.realm.Realm;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
-import static com.prateekj.snooper.utility.TestUtilities.toBytes;
-import static java.net.URI.create;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsSame.sameInstance;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpStatus.OK;
 
 public class AndroidSnooperTest {
 
@@ -44,25 +31,21 @@ public class AndroidSnooperTest {
   @Test
   public void shouldSaveHttpCallViaSpringHttpRequestInterceptor() throws Exception {
     String url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0";
-    URI uri = create(url);
     String responseBody = "responseBody";
     String requestBody = "requestBody";
-    ClientHttpResponse httpResponse = mock(ClientHttpResponse.class);
-    when(httpResponse.getBody()).thenReturn(new ByteArrayInputStream(toBytes(responseBody)));
-    when(httpResponse.getStatusCode()).thenReturn(OK);
-    when(httpResponse.getRawStatusCode()).thenReturn(200);
-    HttpRequest httpRequest = mock(HttpRequest.class);
-    when(httpRequest.getMethod()).thenReturn(POST);
-    when(httpRequest.getURI()).thenReturn(uri);
-    ClientHttpRequestExecution execution = mock(ClientHttpRequestExecution.class);
-    when(execution.execute(httpRequest, toBytes(requestBody))).thenReturn(httpResponse);
 
-    ClientHttpRequestInterceptor interceptor = androidSnooper;
+    HttpCall call = new HttpCallBuilder()
+        .withUrl(url)
+        .withMethod("POST")
+        .withPayload(requestBody)
+        .withResponseBody(responseBody)
+        .withStatusCode(200)
+        .withStatusText("OK").build();
 
-    ClientHttpResponse clientHttpResponse = interceptor.intercept(httpRequest, toBytes(requestBody), execution);
+    androidSnooper.record(call);
+
     HttpCall httpCall = realm.where(HttpCall.class).findAll().first();
 
-    assertThat(clientHttpResponse, sameInstance(httpResponse));
     assertThat(httpCall.getUrl(), is(url));
     assertThat(httpCall.getPayload(), is(requestBody));
     assertThat(httpCall.getMethod(), is("POST"));
--- Snooper/src/main/java/com/prateekj/snooper/AndroidSnooper.java
+++ Snooper/src/main/java/com/prateekj/snooper/AndroidSnooper.java
@@ -3,30 +3,19 @@ package com.prateekj.snooper;
 import android.content.Context;
 
 import com.prateekj.snooper.repo.SnooperRepo;
-import com.prateekj.snooper.transformer.SpringHttpRequestTransformer;
-
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
 
 import io.realm.Realm;
 
-public class AndroidSnooper implements ClientHttpRequestInterceptor{
+public class AndroidSnooper {
 
   private SnooperRepo snooperRepo;
 
   private AndroidSnooper(){}
 
-  @Override
-  public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-    SpringHttpRequestTransformer transformer = new SpringHttpRequestTransformer();
-    ClientHttpResponse clientHttpResponse = execution.execute(request, body);
-    HttpCall httpCall = transformer.transform(request, body, clientHttpResponse);
+  public void record(HttpCall httpCall) throws IOException {
     this.snooperRepo.save(httpCall);
-    return clientHttpResponse;
   }
 
   public static AndroidSnooper init(Context context) {
