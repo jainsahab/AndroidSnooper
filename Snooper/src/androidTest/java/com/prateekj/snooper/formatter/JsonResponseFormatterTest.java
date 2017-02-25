a359cf1cfd9092a0c3465691c8f1b06dcbf843b2
Prateek Jain <prateek.jain23995@yahoo.com>
2017-02-25 23:11:00 +0530

    fixing json formatter to format json array strings

--- Snooper/src/androidTest/java/com/prateekj/snooper/formatter/JsonResponseFormatterTest.java
+++ Snooper/src/androidTest/java/com/prateekj/snooper/formatter/JsonResponseFormatterTest.java
@@ -10,10 +10,19 @@ import static org.junit.Assert.assertThat;
 public class JsonResponseFormatterTest {
 
   @Test
-  public void shouldReturnFormattedJson() throws Exception {
+  public void shouldReturnFormattedJsonObject() throws Exception {
     ResponseFormatter formatter = new JsonResponseFormatter();
     String formattedResponse = formatter.format(readFrom("person_datails_raw_response.json"));
     String expectedResponse = readFrom("person_datails_formatted_response.json");
+
+    assertThat(formattedResponse, is(expectedResponse));
+  }
+
+  @Test
+  public void shouldReturnFormattedJsonArray() throws Exception {
+    ResponseFormatter formatter = new JsonResponseFormatter();
+    String formattedResponse = formatter.format(readFrom("person_datails_raw_array_response.json"));
+    String expectedResponse = readFrom("person_datails_formatted_array_response.json");
 
     assertThat(formattedResponse, is(expectedResponse));
   }
--- Snooper/src/main/java/com/prateekj/snooper/formatter/JsonResponseFormatter.java
+++ Snooper/src/main/java/com/prateekj/snooper/formatter/JsonResponseFormatter.java
@@ -1,17 +1,30 @@
 package com.prateekj.snooper.formatter;
 
+import android.util.Log;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class JsonResponseFormatter implements ResponseFormatter{
+
+  private static final String TAG = JsonResponseFormatter.class.getSimpleName();
+  private static final int INDENT_SPACES = 2;
+
   @Override
   public String format(String response) {
-    String formattedResponse = "";
     try {
-      formattedResponse = new JSONObject(response).toString(2);
+      Object json = new JSONTokener(response).nextValue();
+      if (json instanceof JSONObject) {
+        return new JSONObject(response).toString(INDENT_SPACES);
+      } else if (json instanceof JSONArray) {
+        return new JSONArray(response).toString(INDENT_SPACES);
+      }
     } catch (JSONException e) {
       e.printStackTrace();
+      Log.d(TAG, e.getMessage(), e);
     }
-    return formattedResponse;
+    return "";
   }
 }
