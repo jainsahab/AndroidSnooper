d45d75a0f1a11dacae387115cc277862d53583b9
Prateek Jain <prateek.jain23995@yahoo.com>
2017-02-25 17:15:52 +0530

    missing test for id initializer

---
+++ Snooper/src/androidTest/java/com/prateekj/snooper/model/RealmTestModule.java
@@ -0,0 +1,7 @@
+package com.prateekj.snooper.model;
+
+import io.realm.annotations.RealmModule;
+
+@RealmModule(library = true, allClasses = true)
+public class RealmTestModule {
+}
---
+++ Snooper/src/androidTest/java/com/prateekj/snooper/model/TestModel.java
@@ -0,0 +1,19 @@
+package com.prateekj.snooper.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class TestModel extends RealmObject implements IncrementalIdModel{
+  @PrimaryKey
+  public int id;
+
+  @Override
+  public Class getClazz() {
+    return TestModel.class;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+}
