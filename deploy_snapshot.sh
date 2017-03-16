ba66f1c8e9ab032e396b10b863158381b488d7c4
Prateek Jain <prateek.jain23995@yahoo.com>
2017-03-17 00:00:37 +0530

    adding few flags to deployment script for debugging

--- deploy_snapshot.sh
+++ deploy_snapshot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+set -ev
+if [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then
     echo "Pull request: Skipping snapshot deployment:"
 else
     echo "Deploying Snapshot."
