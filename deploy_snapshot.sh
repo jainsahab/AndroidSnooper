925a6a0e799377b60061f5289c3ca332e4ff2749
Prateek Jain <prateek.jain23995@yahoo.com>
2017-03-16 22:51:58 +0530

    avoiding pull request deployment

---
+++ deploy_snapshot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+    echo "Pull request: Skipping snapshot deployment:"
+else
+    echo "Deploying Snapshot."
+    ./gradlew uploadArchives
+    echo "Deployment done."
+fi
