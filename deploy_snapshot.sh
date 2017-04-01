1ac9653a2a9f9f00c63287023cc09dd90ef4869e
Prayas Jain <p.jain3322@gmail.com>
2017-04-01 12:39:08 +0530

    Correcting the condition of using TRAVIS_PULL_REQUEST environment variable to avoid snapshot deployment

--- deploy_snapshot.sh
+++ deploy_snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo "Pull request: Skipping snapshot deployment:"
 else
     echo "Deploying Snapshot."
