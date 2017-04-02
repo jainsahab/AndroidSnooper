aabedbf78a113d7a32b0f358cb3640fffb3f96c3
Prateek Jain <prateek.jain23995@yahoo.com>
2017-04-02 23:55:52 +0530

    using spoon plugin to debug the potential failure in integration test

--- travis/push-artifact.sh
+++ travis/push-artifact.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-zip -r reports.zip Snooper/build/reports
+zip -r reports.zip Snooper/build/spoon
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
     --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
