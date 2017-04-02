ae4b315411281f00b959dd0b9a4d41a656b7bb2a
Prateek Jain <prateek.jain23995@yahoo.com>
2017-04-02 22:54:42 +0530

    suffixing artifacts with build number when uploading to dropbox

--- travis/push-artifact.sh
+++ travis/push-artifact.sh
@@ -2,6 +2,6 @@
 zip -r reports.zip Snooper/build/reports
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
-    --header "Dropbox-API-Arg: {\"path\": \"/builds/reports.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
+    --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary @reports.zip
