66776c24ad67c6692c551914a19fab1a683dfa8f
Prateek Jain <prateek.jain23995@yahoo.com>
2017-04-18 13:43:40 +0530

    skipping test artifacts deployment for PRs

--- travis/push-artifact.sh
+++ travis/push-artifact.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-zip -r reports.zip Snooper/build/spoon
-curl -X POST https://content.dropboxapi.com/2/files/upload \
-    --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
-    --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @reports.zip
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo "Pull request: Skipping test artifact deployment:"
+else
+    zip -r reports.zip Snooper/build/spoon
+    curl -X POST https://content.dropboxapi.com/2/files/upload \
+        --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
+        --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
+        --header "Content-Type: application/octet-stream" \
+        --data-binary @reports.zip
+fi
