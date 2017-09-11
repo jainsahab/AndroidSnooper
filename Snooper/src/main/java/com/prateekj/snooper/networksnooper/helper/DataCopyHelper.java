bca602b5bc925e1432674e17c4a47a6fc3b53197
Harsh Naraini <hnaraini@thoughtworks.com>
2017-09-11 11:05:47 +0530

    Adding headers in the share data functionality

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/helper/DataCopyHelper.java
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/helper/DataCopyHelper.java
@@ -1,12 +1,18 @@
 package com.prateekj.snooper.networksnooper.helper;
 
+import com.google.common.base.Function;
 import com.prateekj.snooper.formatter.ResponseFormatter;
 import com.prateekj.snooper.formatter.ResponseFormatterFactory;
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord;
 import com.prateekj.snooper.networksnooper.model.HttpHeader;
+import com.prateekj.snooper.networksnooper.model.HttpHeaderValue;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Iterator;
+import java.util.List;
+
+import static com.google.common.collect.Collections2.transform;
 import static com.prateekj.snooper.networksnooper.model.HttpHeader.CONTENT_TYPE;
 
 public class DataCopyHelper {
@@ -33,14 +39,44 @@ public class DataCopyHelper {
   public StringBuilder getHttpCallData() {
     StringBuilder dataToCopy = new StringBuilder();
 
+    appendRequestData(dataToCopy);
+    appendResponseData(dataToCopy);
+    return dataToCopy;
+  }
+
+  private void appendRequestData(StringBuilder dataToCopy) {
     String formattedRequestData = getRequestDataForCopy();
     if (!StringUtils.isEmpty(formattedRequestData)) {
       dataToCopy.append("Request Body");
       dataToCopy.append("\n");
       dataToCopy.append(formattedRequestData);
-      dataToCopy.append("\n");
     }
+    appendHeaders(httpCallRecord.getRequestHeaders(), dataToCopy, "Request Headers");
+  }
 
+  private void appendHeaders(List<HttpHeader> headers, StringBuilder dataToCopy, String heading) {
+    if (headers != null && !headers.isEmpty()) {
+      dataToCopy.append("\n");
+      dataToCopy.append(heading);
+      dataToCopy.append("\n");
+      for (HttpHeader header : headers) {
+        dataToCopy.append(header.getName() + ": ");
+        dataToCopy.append(StringUtils.join(toHeaderValues(header), ";"));
+        dataToCopy.append("\n");
+      }
+    }
+  }
+
+  private Iterator<String> toHeaderValues(HttpHeader httpHeader) {
+    return transform(httpHeader.getValues(), new Function<HttpHeaderValue, String>() {
+      @Override
+      public String apply(HttpHeaderValue headerValue) {
+        return headerValue.getValue();
+      }
+    }).iterator();
+  }
+
+  private void appendResponseData(StringBuilder dataToCopy) {
     String formattedResponseData = getResponseDataForCopy();
 
     if (!StringUtils.isEmpty(formattedResponseData)) {
@@ -48,7 +84,7 @@ public class DataCopyHelper {
       dataToCopy.append("\n");
       dataToCopy.append(formattedResponseData);
     }
-    return dataToCopy;
+    appendHeaders(httpCallRecord.getRequestHeaders(), dataToCopy, "Response Headers");
   }
 
   private String getFormattedData(HttpHeader contentTypeHeader, String dataToCopy) {
